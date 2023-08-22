d45604ed2593ff56f523a92ecff98edd146e6862
tripti-khetan <126143895+tripti-khetan@users.noreply.github.com>
2023-08-22 19:09:50 +0000

    daty.js

---
+++ .devcontainer/basic_js/vari.js
@@ -0,0 +1,12 @@
+const accountId=213442
+// console.log(accountId);
+let name="ramsjsabskc"
+var email="nswjn@2312"
+email="anxkn232"
+
+
+/*
+prefer to not use var 
+becaue issue occur in functional scope and block scope
+*/
+console.table([accountId,name,email])
