b917bdc9dc250e452ba5cf116994b4783d2dea56
tripti-khetan <126143895+tripti-khetan@users.noreply.github.com>
2023-08-22 20:45:25 +0000

    conversion of data

---
+++ .devcontainer/basic_js/dataconv.js
@@ -0,0 +1,14 @@
+// let score="33"
+
+// console.log(typeof(score));
+// let valueInnumber=Number(score)
+// console.log(typeof(valueInnumber));
+
+// let score="33adnsf"
+// console.log(typeof(score));
+// let valueInnumber=Number(score)
+// console.log(valueInnumber);
+
+let isLog=""
+let bolisLog=Boolean(isLog)
+console.log(typeof(bolisLog));
